0fce6904760acfea00cae947735196e32d3a2a85
Damar Wicaksono <damar.wicaksono@gmail.com>
2015-09-22 15:50:45 +0200

    Add a unit test suite for design_srs.py
    - Test whether the number of samples in the design matrix is as specified

---
+++ sa_module/tests/test_design_srs.py
@@ -0,0 +1,26 @@
+"""Unit tests class to test Simple Random Sampling Design generator
+"""
+import unittest
+import env
+import numpy as np
+from samples import design_srs
+
+__author__ = "Damar Wicaksono"
+
+
+class DesignSRSTestCase(unittest.TestCase):
+    """Tests for `design_srs.py`."""
+
+    def setUp(self):
+        """Test fixture build"""
+        self.seed = 94385   # the seed
+        self.n = 123        # 123 samples
+        self.d = 10         # 10-dimensional parameter space
+
+    def test_is_123_the_correct_num_samples(self):
+        """Is 123 is the number of generated samples?"""
+        dm_test = design_srs.create(self.n, self.d, self.seed)
+        self.assertEqual(dm_test.shape[0], self.n)
+
+if __name__ == "__main__":
+    unittest.main()
