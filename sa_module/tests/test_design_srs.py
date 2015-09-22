95e06ff9ceef98e522552bf6039d65d8fad74a2e
Damar Wicaksono <damar.wicaksono@gmail.com>
2015-09-22 16:00:13 +0200

    Add new unit test for design_srs.py
    - Check if the design matrix can be repeated provided the same seed number

--- sa_module/tests/test_design_srs.py
+++ sa_module/tests/test_design_srs.py
@@ -27,5 +27,16 @@ class DesignSRSTestCase(unittest.TestCase):
         dm_test = design_srs.create(self.n, self.d, self.seed)
         self.assertEqual(dm_test.shape[1], self.d)
 
+    def test_is_dm_repeatable(self):
+        """Is the design matrix repeatable given the same seed number"""
+        new_seed = self.seed
+        new_n = self.n
+        new_d = self.d
+        dm_test_1 = design_srs.create(self.n, self.d, self.seed)
+        dm_test_2 = design_srs.create(new_n, new_d, new_seed)
+        for i in range(dm_test_1.shape[0]):
+            for j in range(dm_test_2.shape[1]):
+                self.assertEqual(dm_test_1[i, j], dm_test_2[i, j])
+
 if __name__ == "__main__":
     unittest.main()
