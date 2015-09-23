f7c2583055991b2884ec00b522e34a1724fe6313
Damar Wicaksono <damar.wicaksono@gmail.com>
2015-09-23 13:58:44 +0200

    Update the test for design_lhs for input checking
    All input must be integer

--- sa_module/tests/test_design_lhs.py
+++ sa_module/tests/test_design_lhs.py
@@ -13,4 +13,22 @@ class DesignLHSTestCase(unittest.TestCase):
 
     def setUp(self):
         """Test fixture build"""
-        pass
+        self.seed = 7893457     # Seed number
+        self.n = 100            # Number of samples
+        self.d = 20             # Number of dimension
+        self.dm = design_lhs.create(self.n, self.d, self.seed)
+
+    def test_is_n_other_than_integer_acceptable(self):
+        """Is other than integer for the number of samples acceptable?"""
+        self.assertRaises(TypeError, design_lhs.create, 100.1, self.d, self.seed)
+
+    def test_is_d_other_than_integer_acceptable(self):
+        """Is other than integer for the number of dimensions acceptable?"""
+        self.assertRaises(TypeError, design_lhs, self.n, "abc", self.seed)
+
+    def test_is_seed_other_than_integer_acceptable(self):
+        """Is other than integer for the seed number accepatable?"""
+        self.assertRaises(TypeError, design_lhs, self.n, self.d, 1.234)
+
+if __name__ == "__main__":
+    unittest.main()
