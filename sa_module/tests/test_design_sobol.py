91969699bf87e32a60c81400a4128a59c61e9aac
Damar Wicaksono <damar.wicaksono@gmail.com>
2015-09-23 16:15:20 +0200

    Add bare unit test class for design_sobol.py

---
+++ sa_module/tests/test_design_sobol.py
@@ -0,0 +1,8 @@
+"""Unit test class to test Sobol Sequence design matrix generator
+"""
+import unittest
+import env
+import numpy as np
+from samples import design_sobol
+
+__author__ = "Damar Wicaksono"
