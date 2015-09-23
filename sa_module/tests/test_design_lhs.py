d8fd9366ba30ab62bb0cbc98b941f9a30466184a
Damar Wicaksono <damar.wicaksono@gmail.com>
2015-09-23 13:46:35 +0200

    Add unit test class for design_lhs.py

---
+++ sa_module/tests/test_design_lhs.py
@@ -0,0 +1,16 @@
+"""Unit test class to test the implementation of LHS design generator
+"""
+import unittest
+import env
+import numpy as np
+from samples import design_lhs
+
+__author__ = "Damar Wicaksono"
+
+
+class DesignLHSTestCase(unittest.TestCase):
+    """Tests for design_lhs.py"""
+
+    def setUp(self):
+        """Test fixture build"""
+        pass
