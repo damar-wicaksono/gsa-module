ddf65ee556e4baa8fcf4d5c1441f038563408510
Damar Wicaksono <damar.wicaksono@gmail.com>
2015-09-24 17:56:07 +0200

    Additional tests for design_sobol.py
     - Check if a generated design matrix match previously prepared
       benchmark matrix
     - Check if the element values of the matrix below or equal to 1.0
     - Check if the element values of the matrix greater or equal 0.0

--- sa_module/tests/test_design_sobol.py
+++ sa_module/tests/test_design_sobol.py
@@ -67,17 +67,40 @@ class DesignSobolTestCase(unittest.TestCase):
                           self.genfull, self.numfull)
         design_sobol.makegen("clean", self.testpath)
 
+    def test_is_generator_file_exists(self):
+        """Is the executable for the generator file exist?"""
+        pass
+
+    def test_is_dirnum_file_exists(self):
+        """Is the directional number file exists?"""
+        pass
+
     def test_is_dm_same_as_the_benchmark(self):
         """Is the design matrix match the reference?"""
-        pass
+        design_sobol.makegen("make", self.testpath)
+        dm = design_sobol.create(self.n, self.n, self.genfull, self.numfull)
+        for i in range(self.n):
+            for j in range(self.d):
+                self.assertEqual(dm[i,j], self.benchmark[i,j])
+        design_sobol.makegen("clean", self.testpath)
 
     def test_is_dm_below_or_equal_one(self):
         """Is the element values in the design matrix below 1.0?"""
-        pass
+        design_sobol.makegen("make", self.testpath)
+        dm = design_sobol.create(self.n, self.n, self.genfull, self.numfull)
+        for i in range(self.n):
+            for j in range(self.d):
+                self.assertLessEqual(dm[i,j], 1.0)
+        design_sobol.makegen("clean", self.testpath)
 
     def test_is_dm_above_or_equal_zero(self):
         """Is the element values in the design matrix above 0.0?"""
-        pass
+        design_sobol.makegen("make", self.testpath)
+        dm = design_sobol.create(self.n, self.n, self.genfull, self.numfull)
+        for i in range(self.n):
+            for j in range(self.d):
+                self.assertGreaterEqual(dm[i,j], 0.0)
+        design_sobol.makegen("clean", self.testpath)
 
 if __name__ == "__main__":
     unittest.main()
