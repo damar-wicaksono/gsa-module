af4b77f562f35e410f25ac2e6512300145dcf3d3
Damar Wicaksono <damar.wicaksono@gmail.com>
2015-09-30 11:06:36 +0200

    Update unit test for sobol_saltelli.py
     - Check the `n` and `k` arguments in create() function is positive integers

--- sa_module/tests/test_sobol_saltelli.py
+++ sa_module/tests/test_sobol_saltelli.py
@@ -1,8 +1,8 @@
 """ Unit test class to test Sobol'-Saltelli design matrix generator
 """
 import unittest
-import env
 import numpy as np
+import env
 from sobol import sobol_saltelli
 
 __author__ = 'wicaksono_d'
@@ -15,13 +15,22 @@ class SobolSaltelliTestCase(unittest.TestCase):
         """Test fixture build"""
         self.n = 100
         self.d = 20
-        self.params = [894, 3490]
+        self.params_seed = [894, 3490]
 
     def test_not_acceptable_scheme(self):
         """Is not acceptable scheme handled correctly?"""
         self.assertRaises(NameError, sobol_saltelli.create, self.n, self.d,
-                          "other", self.params)
+                          "other", self.params_seed)
 
+    def test_is_n_other_than_positive_integer_acceptable(self):
+        """Is the number of samples other than positive integer acceptable?"""
+        self.assertRaises(TypeError, sobol_saltelli.create, 10.1, self.d,
+                          "srs", self.params_seed)
+
+    def test_is_k_other_than_positive_integer_acceptable(self):
+        """Is the # of parameters other than positive integer acceptable?"""
+        self.assertRaises(TypeError, sobol_saltelli.create, self.n, -20,
+                          "srs", self.params_seed)
 
 if __name__ == "__main__":
     unittest.main()
