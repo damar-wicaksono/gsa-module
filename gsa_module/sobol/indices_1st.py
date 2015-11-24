1d43c6e0e10fd27f660fc3ff078a2d43e216b858
Damar Wicaksono <damar.wicaksono@gmail.com>
2015-11-24 14:38:29 +0100

    Add module with bare function declarations
    The module will be responsible in calculating the 1st-order
    indices.

--- gsa_module/sobol/indices_1st.py
+++ gsa_module/sobol/indices_1st.py
@@ -3,3 +3,25 @@
 """
 
 __author__ = "Damar Wicaksono"
+
+
+def evaluate(y_dict: dict,
+             estimator="sobol-saltelli",
+             bootstrap=True,
+             bootstrap_n=10000,
+             bootstrap_seed=20151418):
+    """Calculate the 1st-order Sobol' sensitivity indices
+    """
+    pass
+
+
+def janon():
+    """Calculate the 1st-order Sobol' indices using the Janon estimator
+    """
+    pass
+
+
+def sobol_saltelli():
+    """Calculate the 1st-order Sobol' indices using the Sobol'-Saltelli est.
+    """
+    pass
