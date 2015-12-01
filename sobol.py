c04f9584ac7acf9f2908175cb053ae40677bfc47
Damar Wicaksono <damar.wicaksono@gmail.com>
2015-12-01 17:22:45 +0100

    Add driver script to generate sobol sequence samples

--- sobol.py
+++ sobol.py
@@ -9,20 +9,20 @@ __author__ = "Damar Wicaksono"
 def main():
     """Main entry point for the script."""
 
-    n = 100
+    n = 2000
     d = 10
     scheme = "sobol"
     gen_path = "./gsa_module/samples/sobol_seq_gen"
     params_sobol = ["./gsa_module/samples/sobol_seq_gen/sobol.o",
                     "./gsa_module/samples/sobol_seq_gen/new-joe-kuo-6.21201"]
     # make the executable for the generator
-    gsa_module.samples.design_sobol.makegen("make", gen_path)
+    gsa_module.samples.sobol.makegen("make", gen_path)
     dm = gsa_module.sobol.sobol_saltelli.create(n, d, scheme, params_sobol)
     # write down
-    tag = "sobol_100_3"
+    tag = "sobol_{}_{}" .format(n, d)
     gsa_module.sobol.sobol_saltelli.write(dm, tag)
     # clean the executable for the generator
-    gsa_module.samples.design_sobol.makegen("clean", gen_path)
+    gsa_module.samples.sobol.makegen("clean", gen_path)
 
 
 if __name__ == "__main__":
