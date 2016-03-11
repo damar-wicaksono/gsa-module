a608e2de095771e7d4720288c2bf395519eccfeb
Damar Wicaksono <damar.wicaksono@gmail.com>
2016-03-11 17:03:55 +0100

    Make some modules available for import

--- gsa_module/samples/__init__.py
+++ gsa_module/samples/__init__.py
@@ -1,7 +1,10 @@
 """Package containing routines to generate design using various sampling schemes
 """
-from . import srs
+from . import gp_validation
+from . import hammersley
 from . import lhs
 from . import sobol
+from . import srs
+
 
 __author__ = "Damar Wicaksono"
