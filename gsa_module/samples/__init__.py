ed124090947737b5328904f7e5cc5a793e6f91f9
Damar Wicaksono <damar.wicaksono@gmail.com>
2016-08-30 15:03:10 +0200

    Complete the function to parse command line arguments

--- gsa_module/samples/__init__.py
+++ gsa_module/samples/__init__.py
@@ -1,5 +1,6 @@
 """Package containing routines to generate design using various sampling schemes
 """
+from . import cmdln_args
 from . import gp_validation
 from . import hammersley
 from . import lhs
--- gsa_module/samples/cmdln_args.py
+++ gsa_module/samples/cmdln_args.py
@@ -6,4 +6,80 @@ __author__ = "Damar Wicaksono"
 
 def get():
     """Get the passed command line arguments"""
-    pass
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="gsa-module create_sample - Generate Design Matrix File"
+    )
+
+    # the number of samples
+    parser.add_argument(
+        "-n", "--num_samples",
+        type=int,
+        help="The number of samples",
+        required=True
+    )
+
+    # the number of dimension
+    parser.add_argument(
+        "-d", "--num_dimensions",
+        type=int,
+        help="The number of dimensions",
+        required=True
+    )
+
+    # the method to generate sample
+    parser.add_argument(
+        "-m", "--method",
+        type=str,
+        choices=["srs", "lhs"],
+        required=False,
+        default="srs",
+        help="The statistical method to generate sample (default: %(default)s)"
+    )
+
+    # the design matrix filename
+    parser.add_argument(
+        "-o", "--output_file",
+        type=str,
+        required=False,
+        help="The output filename"
+    )
+
+    # The delimiter
+    parser.add_argument(
+        "-sep", "--delimiter",
+        type=str,
+        choices=["csv", "tsv", "ssv"],
+        required=False,
+        default="csv",
+        help="the delimiter for the file (default: %(default)s)"
+    )
+
+    # Get the command line arguments
+    args = parser.parse_args()
+
+    # Check the delimiter
+    if args.delimiter == "csv":
+        delimiter = ","
+    elif args.delimiter == "tsv":
+        delimiter = "\t"
+    else:
+        delimiter = " "
+
+    # Create default filename if not passed
+    if args.output_file is None:
+        output_file = "{}_{}_{}.{}" .format(args.method, args.num_samples,
+                                            args.num_dimensions, args.delimiter)
+    else:
+        output_file = args.output_file
+
+    # Return the parsed command line arguments as a dictionary
+    inputs = {"num_samples": args.num_samples,
+              "num_dimensions": args.num_dimensions,
+              "generation method": args.method,
+              "filename": output_file,
+              "delimiter": delimiter,
+              }
+
+    return inputs
