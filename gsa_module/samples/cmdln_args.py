0e9e727b6a4256563ff74d9195f5d46b04952c7a
Damar Wicaksono <damar.wicaksono@gmail.com>
2016-08-30 16:00:12 +0200

    Add seed number as optional command line arguments
    Add additional error checking

--- gsa_module/samples/cmdln_args.py
+++ gsa_module/samples/cmdln_args.py
@@ -56,8 +56,24 @@ def get():
         help="the delimiter for the file (default: %(default)s)"
     )
 
+    # The random seed number
+    parser.add_argument(
+        "-s", "--seed_number",
+        type=int,
+        required=False,
+        help="The random seed number"
+    )
+
     # Get the command line arguments
     args = parser.parse_args()
+
+    # Check the validity of number of samples
+    if args.num_samples <= 0:
+        raise ValueError
+
+    # Check the validity of the number of dimensions
+    if args.num_dimensions <= 0:
+        raise ValueError
 
     # Check the delimiter
     if args.delimiter == "csv":
@@ -66,6 +82,14 @@ def get():
         delimiter = "\t"
     else:
         delimiter = " "
+
+    # Check the random seed number
+    if args.seed_number is None:
+        seed_number = None
+    elif args.seed_number < 0:
+        raise ValueError
+    else:
+        seed_number = args.seed_number
 
     # Create default filename if not passed
     if args.output_file is None:
@@ -77,9 +101,10 @@ def get():
     # Return the parsed command line arguments as a dictionary
     inputs = {"num_samples": args.num_samples,
               "num_dimensions": args.num_dimensions,
-              "generation method": args.method,
+              "method": args.method,
               "filename": output_file,
               "delimiter": delimiter,
+              "seed_number": seed_number
               }
 
     return inputs
