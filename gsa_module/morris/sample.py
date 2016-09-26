61ce1d3e731127beaf4bf959f262ac137f27ebdd
Damar Wicaksono <damar.wicaksono@gmail.com>
2016-09-26 18:47:19 +0200

    Implement the trajectory method to generate DOE for Morris screening

--- gsa_module/morris/sample.py
+++ gsa_module/morris/sample.py
@@ -27,6 +27,56 @@ def trajectory(r: int, k: int, p: int, numseed: int) -> np.ndarray:
     :param k: the number of parameters
     :param p: the number of levels, have to be an even number
     :param numseed: the seed number for random number generation
-    :return: the trajectory design matrix
+    :return: the trajectory design matrix of dimension r*(k+1)-by-k
     """
-    pass
+    # set the seed number
+    np.random.seed(numseed)
+
+    # Generate B matrix, a strictly lower triangular matrix of 1
+    # See [1] for the properties of B matrix
+    b = np.tril(np.ones([k+1, k], dtype=int), -1)
+
+    # delta is restricted by the number of levels as recommended in [2] to
+    # ensure equally probable parameter space coverage
+    delta = p / 2 / (p - 1)
+    # Create a diagonal matrix with delta in the diagonal
+    delta_diag = np.diag([delta for _ in range(k+1)])
+
+    # Initialize B_star matrix
+    b_star = np.empty([r*(k+1), k])
+
+    # Create J matrix, (k + 1)-by-k matrix of 1
+    j_k = np.ones([k+1, k])
+
+    # Generate r number of trajectories, each contains k + 1 evaluation points
+    for i in range(r):
+
+        # Generate random starting point, x_star
+        # Note that not all levels can be selected according to [1]
+        # Only the first half of the parameter space is valid to be selected
+        x_star = np.empty([k+1, k])
+        # Fill in the starting point dimension-by-dimension
+        for j in range(k):
+            x_star[:, j] = np.random.choice(np.arange(p/2) / (p - 1))
+
+        # Generate random permutation matrix, p_star
+        # See [1] for the properties of the p_star matrix
+        permuted = np.random.permutation(k)
+        p_star = np.zeros([k, k])
+        for j in range(k):
+            p_star[j, permuted[j]] = 1
+
+        # Generate random direction matrix, D_star
+        # See [1] for the properties of the D_star matrix
+        d_star = np.diag([np.random.choice([-1, 1]) for _ in range(k)])
+
+        # Set indices that signify a trajectory in the matrix
+        index_list = np.arange(k+1) + i * (k+1)
+
+        # Compute the b_star for each trajectories
+        b_star[index_list, :] = x_star + np.dot(delta_diag/2,
+                                                np.dot((np.dot((2*b - j_k),
+                                                               d_star) + j_k),
+                                                       p_star))
+
+    return b_star
