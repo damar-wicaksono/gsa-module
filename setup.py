3cd85add36999306a24fe9f20d1a513c4f03db90
Damar Wicaksono <damar.wicaksono@gmail.com>
2016-09-27 16:42:58 +0200

    Revise the setup script

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 from setuptools import setup
 
 
-setup(name="gsa-module",
-      version="0.5.2",
-      description="Package to conduct global sensitivity analysis of computer simulation",
-      url="https://bitbucket.org/damar-wicaksono/gsa-module",
-      author="Damar Wicaksono",
-      author_email="damar.wicaksono@gmail.com",
-      license="MIT",
-      packages=["gsa_module"],
-      scripts=["bin/create_sample", "bin/create_validset"],
-      zip_safe=False)
+setup(
+    name="gsa-module",
+    version="0.5.2",
+    description="Package to conduct global sensitivity analysis of computer simulation",
+    url="https://bitbucket.org/damar-wicaksono/gsa-module",
+    author="Damar Wicaksono",
+    author_email="damar.wicaksono@gmail.com",
+    license="MIT",
+    packages=["gsa_module"],
+    scripts=["bin/create_sample", "bin/create_validset"],
+
+    # Provide the following executable scripts on the path
+    entry_points={
+        "console_scripts": [
+            "gsa_morris_generate=gsa_module.cmdln_interface:morris_generate"
+        ]
+    },
+      zip_safe=False
+)
