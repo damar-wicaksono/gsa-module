af554159a5b333dd70bc8fb662699fe88a204c43
Damar Wicaksono <damar.wicaksono@gmail.com>
2016-09-27 11:27:53 +0200

    Add modules import statement in Morris sub-package

--- gsa_module/morris/__init__.py
+++ gsa_module/morris/__init__.py
@@ -6,4 +6,5 @@
     Set of routines to conduct Morris screening analysis for qualitatively rank
     input parameter importance in global sensitivity analysis setting
 """
-
+from . import cmdln_args
+from . import sample
