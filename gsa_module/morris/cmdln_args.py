e13e744629c649dcc1a9f8d47c2501558abe00b1
Damar Wicaksono <damar.wicaksono@gmail.com>
2016-09-27 10:27:57 +0200

    Add bare module for routines handling command line arguments for Morris

---
+++ gsa_module/morris/cmdln_args.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    gsa_module.morris.cmdnln_args
+    *****************************
+
+    Module with routines to parse different command line operations of carrying
+    out Morris screening analysis, namely: generate samples, analyze samples
+    (make ranking), and make some plots (exploratory purpose, not for
+    publication)
+"""
