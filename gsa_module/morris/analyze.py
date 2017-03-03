23f54898f805071ba7ad99278b6f2f04e17d4bf1
Damar Wicaksono <damar.wicaksono@gmail.com>
2017-03-03 18:15:59 +0100

    Add bare python module for analyzing Morris output

---
+++ gsa_module/morris/analyze.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+    gsa_module.morris.analyze
+    *************************
+
+    Module implementing method to analyze output computed using one
+    of the screening design of experiment.
+    The importance of parameter can be assessed based on the statistics
+    of their elementary effects.
+    The computation of the elementary effects differs for the two different
+    design. Both of them can also be standardized if necessary.
+"""
