6bd38be479e3f7e0a3e58d04ee20455ac96abe37
Damar Wicaksono <damar.wicaksono@gmail.com>
2017-03-03 15:31:13 +0100

    Fix randomize sobol command line interface
    The command line argument to randomize sobol is now implemented. The seed number can be specified if Sobol' sequence is to be randomize by random shifting.

--- gsa_module/cmdln_interface.py
+++ gsa_module/cmdln_interface.py
@@ -9,6 +9,43 @@
 import numpy as np
 
 
+def create_sample():
+    """gsa-module, create a design of experiment command line interface"""
+    from gsa_module import samples
+
+    # Get the command line arguments
+    inputs = samples.cmdln_args.get_create_sample()
+
+    # Generate the design matrix file
+    if inputs["method"] == "srs":
+        # Create simple random sampling design
+        dm = samples.srs.create(inputs["num_samples"], inputs["num_dimensions"],
+                                seed=inputs["seed_number"])
+    elif inputs["method"] == "lhs":
+        # Create Latin Hypercube Sampling design
+        dm = samples.lhs.create(inputs["num_samples"], inputs["num_dimensions"],
+                                seed=inputs["seed_number"])
+    elif inputs["method"] == "sobol":
+        # Create Sobol' quasirandom sequence design
+        dm = samples.sobol.create(inputs["num_samples"],
+                                  inputs["num_dimensions"],
+                                  generator=inputs["sobol_generator"],
+                                  dirnumfile=inputs["direction_numbers"],
+                                  incl_nom=inputs["include_nominal"],
+                                  randomize=inputs["randomize_sobol"],
+                                  seed=inputs["seed_number"])
+    elif inputs["method"] == "lhs-opt":
+        # Create an optimized latin hypercube design
+        dm = samples.lhs_opt.create_ese(inputs["num_samples"],
+                                        inputs["num_dimensions"],
+                                        seed=inputs["seed_number"],
+                                        max_outer=inputs["num_iterations"])
+
+    # Save the design into file
+    np.savetxt(inputs["filename"], dm,
+               fmt="%1.6e", delimiter=inputs["delimiter"])
+
+
 def morris_generate():
     """gsa-module, create Morris experimental design command line interface"""
     from gsa_module import morris
