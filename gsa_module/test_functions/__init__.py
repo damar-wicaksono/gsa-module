7adea41713e6bc8847314f9a6f58700cb9511628
Damar Wicaksono <damar.wicaksono@gmail.com>
2017-03-07 15:24:02 +0100

    Impelement Ishigami test function

---
+++ gsa_module/test_functions/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+    gsa_module.test_functions
+    ~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Set of python3 implementation of various analytical test functions for
+    verifying the global sensitivity analysis method
+"""
+from . import ishigami
