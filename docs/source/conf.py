a2cf92f4f76a93379c23c10ef2cf7bb35a1967fd
Damar Wicaksono <damar.wicaksono@gmail.com>
2017-03-07 16:19:13 +0100

    Change the up appearance of sphinx-doc html

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -118,7 +118,7 @@ html_theme = 'alabaster'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {"show_related": True}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
@@ -159,7 +159,15 @@ html_static_path = ['_static']
 
 # Custom sidebar templates, maps document names to template names.
 #html_sidebars = {}
-
+html_sidebars = {
+    '**': [
+        'about.html',
+        'navigation.html',
+        'relations.html',
+        'searchbox.html',
+        'donate.html',
+    ]
+}
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
 #html_additional_pages = {}
