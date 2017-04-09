9dc3abeea1397cbd9190afb8530942ca1b10c771
Damar Wicaksono <damar.wicaksono@psi.ch>
2017-04-09 18:54:51 +0200

    Implement function to estimate first- and total order sensitivity

--- gsa_module/sobol/indices_1st.py
+++ gsa_module/sobol/indices_1st.py
@@ -1,22 +1,40 @@
 # -*- coding: utf-8 -*-
-"""indices_1st.py: Module to calculate the 1st-order Sobol' indices
+"""
+    gsa_module.sobol.indices_1st
+    ****************************
+    
+    Module with functions to calculate the 1st-order Sobol' indices
 """
 import numpy as np
 
 __author__ = "Damar Wicaksono"
 
 
-def evaluate(y_dict: dict, estimator="sobol-saltelli") -> dict:
+def evaluate(y_dict: dict, estimator: str="sobol-saltelli") -> np.ndarray:
     """Calculate the 1st-order Sobol' sensitivity indices and create a dict
 
     This is a driver function to call several choices of 1st-order index
     estimators. The input is a dictionary of output vectors.
 
     :param y_dict: a dictionary of numpy array of model outputs
-    :param estimator: (str) which estimator to use
-    :return: a dictionary of all the indices
+    :param estimator: which estimator to use
+    :return: a numpy array of all the first-order indices
     """
-    pass
+    # Get some common parameters
+    num_dimensions = len(y_dict) - 2
+
+    # Compute the 1st-order sensitivity indices
+    si = np.empty(num_dimensions)
+    if estimator == "sobol-saltelli":
+        for i in range(num_dimensions):
+            key = "ab_{}" .format(i+1)
+            si[i] = sobol_saltelli(y_dict["a"], y_dict["b"], y_dict[key])
+    elif estimator == "janon":
+        for i in range(num_dimensions):
+            key = "ab_{}" .format(i+1)
+            si[i] = janon(y_dict["b"], y_dict[key])
+
+    return si
 
 
 def bootstrap(y_dict: dict, estimator="sobol-saltelli",
@@ -58,7 +76,15 @@ def janon(fb: np.ndarray, fab_i: np.ndarray) -> float:
     :param fab_i: numpy array of model output with matrix AB_i
     :return: (float) the 1st-order index for parameter-i
     """
-    pass
+    # Compute the squared mean according to Janon et al. formulation
+    mean_squared = (np.mean((fb + fab_i)/2))**2
+
+    nominator = np.mean(fb * fab_i) - mean_squared
+    denominator = np.mean((fb**2 + fab_i**2)/2) - mean_squared
+
+    si = nominator / denominator
+
+    return si
 
 
 def sobol_saltelli(fa: np.ndarray, fb: np.ndarray, fab_i: np.ndarray) -> float:
@@ -74,12 +100,21 @@ def sobol_saltelli(fa: np.ndarray, fb: np.ndarray, fab_i: np.ndarray) -> float:
         output. Design and estimator for the total sensitivity index,"
         Computer Physics Communications, 181, pp. 259-270, 2010
     (2) A. Saltelli, "Making best use of model evaluations to compute
-        sensitivity indices," Computer Physics Communications, 145, pp. 280-297,
-        2002
+        sensitivity indices," Computer Physics Communications, 145, 
+        pp. 280-297, 2002
 
-    :param fa: numpy array of model output with matrix A
-    :param fb: numpy array of model output with matrix B
+    :param fa: numpy array of model output evaluated with input matrix A
+    :param fb: numpy array of model output evaluated with input matrix B
     :param fab_i: numpy array of model output with matrix AB_i
-    :return: the 1st-order index for parameter-i
+    :return: the first order sensitivity of parameter i
     """
-    pass
+    # Compute the Squared Mean (f(a) * f(b))
+    mean_squared = np.mean(fa * fb)
+
+    # Compute the Variance
+    var = np.var(fa, ddof=1)
+
+    # Compute the first order sensitivity
+    si = (np.mean(fb * fab_i) - mean_squared) / var
+
+    return si
--- gsa_module/sobol/indices_total.py
+++ gsa_module/sobol/indices_total.py
@@ -1,5 +1,45 @@
 # -*- coding: utf-8 -*-
-"""indices_total.py: Module to calculate the total-order Sobol' indices
 """
+    gsa_module.sobol.indices_total
+    ******************************
+    
+    Module with functions to calculate the total-order Sobol' indices
+"""
+import numpy as np
 
 __author__ = "Damar Wicaksono"
+
+
+def evaluate(y_dict: dict, estimator: str="jansen") -> dict:
+    """Calculate the total-order Sobol' sensitivity indices
+    
+    :param y_dict: a dictionary of numpy array of model outputs
+    :param estimator: which estimator to use
+    :return: a numpy array of all the total-order indices
+    """
+    # Get some common parameters
+    num_dimensions = len(y_dict) - 2
+
+    # Compute the 1st-order sensitivity indices
+    sti = np.empty(num_dimensions)
+    if estimator == "jansen":
+        for i in range(num_dimensions):
+            key = "ab_{}" .format(i+1)
+            sti[i] = jansen(y_dict["a"], y_dict[key])
+
+    return sti
+
+
+def jansen(fa: np.ndarray, fab_i: np.ndarray) -> float:
+    """Calculate the total-order Sobol' sensitivity indices using Jansen est. 
+    
+    :param fa: numpy array of model output evaluated with input matrix A
+    :param fab_i: numpy array of model output with matrix AB_i
+    :return: the total order sensitivity of parameter i
+    """
+    # Compute the Variance
+    var = np.var(fa, ddof=1)
+
+    sti = 0.5 * np.mean((fa - fab_i)**2) / var
+
+    return sti
